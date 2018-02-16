efd0a121f93e2387bfb275f5cd676b49940c97ad
Daniëlle Duijst <dmsduijst@gmail.com>
2018-02-16 15:29:06 +0100

    Guest updated

---
+++ src/main/java/com/capgemini/Guest.java
@@ -0,0 +1,24 @@
+package com.capgemini;
+
+public class Guest {
+
+    private String firstName;
+
+    private String lastName;
+
+    private int passportNumber;
+
+    private int phoneNumber;
+
+    private String emailAddress;
+
+    public Guest (String firstName, String lastName, int passportNumber, int phoneNumber, String emailAddress){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.passportNumber = passportNumber;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+    }
+
+    
+}
