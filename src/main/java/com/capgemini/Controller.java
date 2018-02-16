79a34c9d73319e572ddb4917d728d249139a6914
Daniëlle Duijst <dmsduijst@gmail.com>
2018-02-16 15:20:38 +0100

    Controller aangemaakt

---
+++ src/main/java/com/capgemini/Controller.java
@@ -0,0 +1,21 @@
+package com.capgemini;
+
+public class Controller {
+
+    //private Guest guest
+
+    //private Room room
+
+    //private Staff staff
+
+    //private Booking booking
+
+
+    // -----------------------
+
+    // public void checkIn();
+
+    // public void checkOut();
+
+
+}
