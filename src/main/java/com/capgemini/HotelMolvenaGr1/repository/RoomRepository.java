6e78902c6c3bad3c1eb81830589996a06ea205f2
suuzels <suzannevdmeer@gmail.com>
2018-02-27 16:42:08 +0100

    Available naar Occupied veranderd

--- src/main/java/com/capgemini/HotelMolvenaGr1/repository/RoomRepository.java
+++ src/main/java/com/capgemini/HotelMolvenaGr1/repository/RoomRepository.java
@@ -20,21 +20,21 @@ public class RoomRepository {
 
         Room room1 = new Room();
         room1.setRoomType(ERoomType.STANDARD);
-        room1.setAvailable(true);
+        room1.setOccupied(false);
         room1.setRoomID("1");
         room1.setDefaultPrice(89);
         room1.setRoomName("Iron Man");
 
         Room room2 = new Room();
         room2.setRoomType(ERoomType.LUXURY);
-        room2.setAvailable(true);
+        room2.setOccupied(false);
         room2.setRoomID("2");
         room2.setDefaultPrice(149);
         room2.setRoomName("Black Panther");
 
         Room room3 = new Room();
         room3.setRoomType(ERoomType.HONEYMOON_SUITE);
-        room3.setAvailable(true);
+        room3.setOccupied(false);
         room3.setRoomID("3");
         room3.setDefaultPrice(345);
         room3.setRoomName("Hulk");
