ac6177362b8b5f09a66369eed4561481f87e7c28
suuzels <suzannevdmeer@gmail.com>
2018-03-01 13:58:17 +0100

    .

---
+++ src/main/java/com/capgemini/HotelMolvenaGr1/service/BookingService.java
@@ -0,0 +1,25 @@
+package com.capgemini.HotelMolvenaGr1.service;
+
+import com.capgemini.HotelMolvenaGr1.model.Booking;
+import com.capgemini.HotelMolvenaGr1.repository.BookingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class BookingService {
+
+    @Autowired
+    private BookingRepository bookingRepository;
+
+    @PostConstruct
+    public void init(){
+        Booking b1 = new Booking();
+
+        this.bookingRepository.save(b1);
+
+    }
+}
