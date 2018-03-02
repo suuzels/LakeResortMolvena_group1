83178029f1901e946f231d1930b39aed923d8d3d
Daniëlle Duijst <dmsduijst@gmail.com>
2018-03-02 15:03:37 +0100

    oude code weg

--- src/main/java/com/capgemini/HotelMolvenaGr1/repository/RoomRepository.java
+++ src/main/java/com/capgemini/HotelMolvenaGr1/repository/RoomRepository.java
@@ -11,43 +11,4 @@ import java.util.ArrayList;
 public interface RoomRepository extends CrudRepository<Room, Long> {
 
 
-
-//
-//
-//
-//    public ArrayList<Room> getRooms() {
-//        return rooms;
-//    }
-//
-//    public Room changeRoomType(Room room, ERoomType roomType) {
-//        this.room.setRoomType(roomType);
-//        return room;
-//    }
-//
-//    public void deleteRoom() {
-//        for (Room room : rooms) {
-//            rooms.remove(room);
-//        }
-//    }
-//
-//    public void changeRoom(String roomID, ERoomType roomType) {
-//        for (Room room : this.rooms) {
-//            if (room.getRoomID() == roomID) {
-//                room.setRoomType(roomType);
-//            }
-//        }
-//
-//    }
-//
-//    // Getters setters
-//
-//    public ArrayList<Room> getRoomList() {
-//        return rooms;
-//    }
-//
-//    public void setRoomList(ArrayList<Room> roomList) {
-//        this.rooms = roomList;
-//    }
-
-
 }
