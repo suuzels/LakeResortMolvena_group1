88fcb11801b7299229dd26408c79259852479526
suuzels <suzannevdmeer@gmail.com>
2018-03-09 11:56:24 +0100

    transactional geimporteerd

--- src/main/java/com/capgemini/HotelMolvenaGr1/service/BookingService.java
+++ src/main/java/com/capgemini/HotelMolvenaGr1/service/BookingService.java
@@ -8,6 +8,8 @@ import com.capgemini.HotelMolvenaGr1.repository.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
 public class BookingService {
 
